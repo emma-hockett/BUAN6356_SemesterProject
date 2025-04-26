c02afb1e6b037b215f7b0dd85f8139990a443297
Emma Hockett <146998972+emma-hockett@users.noreply.github.com>
2025-04-26 10:12:30 -0500

    comments

--- 6356_LogisticModelTesting.R
+++ 6356_LogisticModelTesting.R
@@ -6,6 +6,7 @@
 ################################################################################
 
 # Install and load necessary packages
+install.packages("smotefamily")
 install.packages("caret")
 library(caret)
 library(smotefamily)
@@ -19,6 +20,7 @@ test_data <- HR_Employee_df[test_indicies, ]
 train_data <- HR_Employee_df[-test_indicies, ]
 
 # Adding SMOTE to the training set to counteract the class imbalance of the target variable 
+# This should ensure the minority class is more fairly considered while training the data
 train_data <- train_data %>% mutate_if(is.character, as.factor) %>% mutate_if(is.factor, as.numeric)
 test_data <- test_data %>% mutate_if(is.character, as.factor) %>% mutate_if(is.factor, as.numeric)
 train_data_smote <- SMOTE(train_data[,-which(names(train_data) == "Attrition")], train_data$Attrition, K=5, dup_size = 2)
@@ -34,29 +36,20 @@ combined_data$class <- NULL
 best_cutoff <- function(model, test_data, cutoff = seq(0.1, 0.9, by = 0.05)) {
   
   pHat <- predict(model, test_data, type = "response")
-  
   best_acc <- 0
-  
   best_cutoff <- 0
   
   for (cut in cutoff) {
     
     yHat <- ifelse(pHat >= cut, 1, 0)
-    
     acc <- 100 * mean(yHat == test_data$Attrition)
     
     if (acc > best_acc) {
-      
       best_acc <- acc
-      
       best_cutoff <- cut
-      
     }
-    
   }
-  
   return(list(best_cutoff = best_cutoff, best_accuracy = best_acc))
-  
 }
 
 ####################### Model Evaluation Function ##############################
@@ -64,33 +57,29 @@ best_cutoff <- function(model, test_data, cutoff = seq(0.1, 0.9, by = 0.05)) {
 eval_model <- function(model, cutoff, test_data) {
   
   pHat <- predict(model, test_data, type = "response")
-  
   yHat <- ifelse(pHat >= cutoff, 1, 0)
   
   TP <- sum(yHat == 1 & test_data$Attrition == 1)
-  
   TN <- sum(yHat == 0 & test_data$Attrition == 0)
-  
   FP <- sum(yHat == 1 & test_data$Attrition == 0)
-  
   FN <- sum(yHat == 0 & test_data$Attrition == 1)
   
   accuracy <- 100 * mean(yHat == test_data$Attrition)
-  
   sensitivity <- 100 * (TP / (TP + FN))
-  
   specificity <- 100 * (TN / (TN + FP))
-  
   return(list(accuracy = accuracy, sensitivity = sensitivity, specificity = specificity))
-  
 }
 
 
 #################### Evaluating Combination Function ###########################
 
+# WARNING: Would not recommend running the evaluate_combinations function!
+# This function can take some time to run through because it is testing so many combinations and cutoff values.
+# We included the top results in the report, if you want to run this you can it will just take some time to run through.
+
+
 # List of top predictors to iterate through 
 predictors <- c("Overtime", "Performance_Rating", "Percent_Salary_Hike", "Joining_Year", "Age", "Years_At_Company", "Job_Level", "Monthly_Income", "Total_Working_Years", "Business_Travel")
-
 
 # Function to evaluate all combinations of predictors
 evaluate_combinations <- function(predictors, train_data, test_data) {
@@ -126,31 +115,46 @@ print(head(top_results, 10))  # Display the top 10 combinations based on accurac
 
 
 #################### Precision and F1 Score Metrics ############################
-# After finding the best equations, 3 achieved the same accuracy, sensitivity, and specifity. To try and break the tie testinf precision and F1
-formula <- "Attrition ~ Performance_Rating + Joining_Year + Years_At_Company + Stock_Option + Years_With_Manager"
-model <- glm(formula, family = binomial, data = combined_data)
-cutoff_results <- best_cutoff(model, test_data)
-pHat <- predict(model, test_data, type = "response")
-yHat <- ifelse(pHat >= cutoff_results$best_cutoff, 1, 0)
-TP <- sum(yHat == 1 & test_data$Attrition == 1)
-TN <- sum(yHat == 0 & test_data$Attrition == 0)
-FP <- sum(yHat == 1 & test_data$Attrition == 0)
-FN <- sum(yHat == 0 & test_data$Attrition == 1)
-accuracy <- 100 * mean(yHat == test_data$Attrition)
-sensitivity <- 100 * (TP / (TP + FN))
-specificity <- 100 * (TN / (TN + FP))
-precision <- 100*(TP/ (TP +FP))
-precision
-f1 <- 2*((precision * sensitivity) / (precision + sensitivity))
-f1
+# After finding the best equations, 3 achieved the same accuracy, sensitivity, and specifity. To try to break the tie testing precision and F1
+# To test the Precision or F1-Score you can test the three major equations: 
+# Equation 6: "Attrition ~ Performance_Rating + Percent_Salary_Hike + Joining_Year + Years_At_Company + Years_In_Role"
+# Equation 7: "Attrition ~ Performance_Rating + Joining_Year + Years_At_Company + Years_In_Role + Stock_Option"
+# Equation 8: "Attrition ~ Performance_Rating + Joining_Year + Years_At_Company + Stock_Option + Years_With_Manager"
+
+formulas <- list( "Attrition ~ Performance_Rating + Percent_Salary_Hike + Joining_Year + Years_At_Company + Years_In_Role",
+                  "Attrition ~ Performance_Rating + Joining_Year + Years_At_Company + Years_In_Role + Stock_Option",
+                  "Attrition ~ Performance_Rating + Joining_Year + Years_At_Company + Stock_Option + Years_With_Manager")
+
+prec_f1 <- data.frame(Formula = character(), Precision = numeric(), F1 = numeric()) 
+                
+for (f in formulas){
+  model <- glm(as.formula(f), family = binomial, data = combined_data)
+  cutoff_results <- best_cutoff(model, test_data)
+  pHat <- predict(model, test_data, type = "response")
+  yHat <- ifelse(pHat >= cutoff_results$best_cutoff, 1, 0)
+  TP <- sum(yHat == 1 & test_data$Attrition == 1)
+  TN <- sum(yHat == 0 & test_data$Attrition == 0)
+  FP <- sum(yHat == 1 & test_data$Attrition == 0)
+  FN <- sum(yHat == 0 & test_data$Attrition == 1)
+  precision <- 100*(TP/ (TP +FP))
+  sensitivity <- 100 * (TP / (TP + FN))
+  f1 <- 2*((precision * sensitivity) / (precision + sensitivity))
+  
+  prec_f1 <- rbind(prec_f1, data.frame(Formula = f, Precision = precision, F1 = f1))
+}
+
+prec_f1
 
 
 ####################### Cross Fold Validation Test #############################
 # After precision and F1 Scores, the three equations are still tied so testing their stability using k fold validation 
+cv_result <- data.frame(Formula = character(), Avg_Accuracy = numeric())
 
-formula <- "Attrition ~ Performance_Rating + Joining_Year + Years_At_Company + Percent_Salary_Hike + Years_In_Role"
-set.seed(123)
-train_control <- trainControl(method = "cv", number = 10)
-cv_model <- train(as.formula(formula), data = combined_data, method = "glm", family = binomial, trControl = train_control)
-cv_model$results
+for (f in formulas) {
+  set.seed(123)
+  train_control <- trainControl(method = "cv", number = 10)
+  cv_model <- train(as.formula(f), data = combined_data, method = "glm", family = binomial, trControl = train_control)
+  cv_result <- rbind(cv_result, data.frame(Formula = f, Avg_Accuracy = cv_model$results$Accuracy))
+}
 
+cv_result
